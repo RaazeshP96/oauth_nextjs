56088209f97f08aa274cc7675dcb2fb6dd38e1f4
raazeshp96 <raazeshp96@gmail.com>
2023-09-19 06:46:27 +0545

    fix:added Login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,17 @@
-import { NextPage } from "next";
-import Link from "next/link";
-import React from "react";
+import { LoginForm } from "./form";
+import Header from "@/components/header.component";
 
-const Login: NextPage = () => {
+export default function LoginPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-12">
-      <div className="text-orange-300 font-bold text-xl">Login</div>
-      <div className="flex">
-        <div>Hosted in:</div>
-        <Link href="https://oauth-nextjs-tau.vercel.app/" target="_blank">
-          Vercel
-        </Link>
-      </div>
-    </main>
+    <>
+      <Header />
+      <section className="bg-ct-blue-600 min-h-screen pt-20">
+        <div className="container mx-auto px-6 py-12 h-full flex justify-center items-center">
+          <div className="md:w-8/12 lg:w-5/12 bg-white px-8 py-10">
+            <LoginForm />
+          </div>
+        </div>
+      </section>
+    </>
   );
-};
-
-export default Login;
+}
